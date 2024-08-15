7743a7ca9879cee955ba07271f28b62cd21023a1
ADEILTON PEREIRA ALVES <70703098+AdeiltonPereiraAlves@users.noreply.github.com>
2024-08-15 20:00:55 -0300

    fist commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['http://localhost:3001/api/produtos','images.kabum.com.br'] // Substitua pelo domínio do seu backend
+        domains: ['http://localhost:3001/api/produtos','images.kabum.com.br','img.terabyteshop.com.br'] // Substitua pelo domínio do seu backend
       },
       webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         // Modifique a configuração do Webpack aqui, se necessário
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,7 +16,7 @@ export default function Card(props: CardProps){
     return (
         <div className="flex flex-col gap-2 p-4 ">
            <h1>{props.nome}</h1>
-           <Image src={props.img} alt={props.nome} width={300} height={300}/>
+           <Image src={props.img} alt={props.nome} width={100} height={100}/>
            <p>{props.descricao}</p>
            <h4>{props.preco}</h4>
         </div>
